8bae1e8e83087335a154dfbd087f343fcf6c64da
HYUNSUK331 <rjqnrdl331@naver.com>
2022-05-03 13:14:07 +0900

    코드업 기초 100제 24번(Java)

---
+++ 1023_25462450(AC).java
@@ -0,0 +1,14 @@
+﻿import java.util.Scanner;
+ 
+public class Main{
+    public static void main(String args[]){
+        String a;
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextLine();    
+        
+        String []b = a.split("\\.");
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+    }
+}
