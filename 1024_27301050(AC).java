7c9932144b81d124515042afc4e873bc314c764c
HYUNSUK331 <rjqnrdl331@naver.com>
2022-07-21 23:04:47 +0900

    코드업 기초 100제 24번(Java)수정

--- 1024_27301050(AC).java
+++ 1024_27301050(AC).java
@@ -3,8 +3,8 @@
 public class Main{
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        String[] arr = a.split("");
+        String b = sc.nextLine();
+        String[] arr = b.split("");
         for(int i=0; i<arr.length; i++){
             System.out.println("'"+arr[i]+"'");
         }
