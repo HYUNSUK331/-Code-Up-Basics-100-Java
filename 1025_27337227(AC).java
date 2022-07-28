5891686c3cc5943717c3ee520f00d142b1706f96
HYUNSUK331 <rjqnrdl331@naver.com>
2022-07-28 20:21:53 +0900

    코드업 기초 100제 25번(java)

---
+++ 1025_27337227(AC).java
@@ -0,0 +1,18 @@
+﻿import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		char[] c = str.toCharArray();
+		
+		System.out.println("[" + c[0] + "0000]");
+		System.out.println("[" + c[1] + "000]");
+		System.out.println("[" + c[2] + "00]");
+		System.out.println("[" + c[3] + "0]");
+		System.out.println("[" + c[4] + "]");
+		
+	}
+}
