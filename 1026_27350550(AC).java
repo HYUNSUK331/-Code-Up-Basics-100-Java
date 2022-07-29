6e1366f5d20ac1585d0dac501f96067445f6d803
HYUNSUK331 <rjqnrdl331@naver.com>
2022-07-29 22:30:07 +0900

    코드업 기초 100제 26번(java)

---
+++ 1026_27350550(AC).java
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        String s;
+        Scanner sc = new Scanner(System.in);
+        s=sc.next();
+        
+        String[] sr = s.split(":");
+        
+        System.out.println(Integer.valueOf(sr[1]));
+        
+    }
+}
