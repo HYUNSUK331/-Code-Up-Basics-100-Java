bb4d8095d18972fcad8cbff774d0a3041e3385c8
HYUNSUK331 <rjqnrdl331@naver.com>
2022-07-31 23:49:06 +0900

    코드업 기초 100제 27번(java)

---
+++ 1027_27368394(AC).java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String time = sc.nextLine();
+        
+        StringTokenizer st = new StringTokenizer(time,".");
+        
+        String [] times = new String[4];
+        
+        int i=0;    
+        while(st.hasMoreTokens()) {
+            times[i] = st.nextToken();
+            i+=1;
+        }
+        
+        System.out.print(times[2]+"-"+times[1]+"-"+times[0]);
+        
+    }
+}
