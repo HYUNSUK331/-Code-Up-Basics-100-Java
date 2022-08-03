1bac727e0b1a2bf141e02253744b97779f1c180c
HYUNSUK331 <rjqnrdl331@naver.com>
2022-08-03 23:45:32 +0900

    코드업 기초100제 28번 (java)

---
+++ 1028_27402906(AC).java
@@ -0,0 +1,12 @@
+﻿import java.util.Scanner;
+ 
+public class Main{
+    public static void main(String args[]){
+        Long a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextLong();    
+        
+        System.out.printf("%d",a);
+    }
+}
+
